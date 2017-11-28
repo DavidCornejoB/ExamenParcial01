7ad5fb1d8dc9140906d78a7bab3bcd1f02a57ede
User <User@172.16.223.58>
2017-11-28 08:08:39 -0500

    crea clase abstracta persona

---
+++ ExamenParcial01/src/examenparcial01/modelo/Persona.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examenparcial01.modelo;
+
+/**
+ *
+ * @author User
+ */
+public abstract class Persona {
+    private String nombre;
+    private String apellido;
+    private String fechaNac;
+
+    public Persona(String nombre, String apellido, String fechaNac) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaNac = fechaNac;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getFechaNac() {
+        return fechaNac;
+    }
+
+    public void setFechaNac(String fechaNac) {
+        this.fechaNac = fechaNac;
+    }
+    
+    
+}
