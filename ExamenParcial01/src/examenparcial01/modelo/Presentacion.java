888307d1247c4e448473ffeaf03aff196ca73393
User <User@172.16.223.58>
2017-11-28 08:38:19 -0500

    crea termina la clase  Presentacion con getters y setters

--- ExamenParcial01/src/examenparcial01/modelo/Presentacion.java
+++ ExamenParcial01/src/examenparcial01/modelo/Presentacion.java
@@ -13,6 +13,45 @@ public class Presentacion {
     
     private Festival fest;
     private Artista artista;
+    private int numeroPres;
+
+    public Presentacion(Festival fest, Artista artista, int numeroPres) {
+        this.fest = fest;
+        this.artista = artista;
+        this.numeroPres = numeroPres;
+    }
+            
+            
+
+    public Festival getFest() {
+        return fest;
+    }
+
+    public void setFest(Festival fest) {
+        this.fest = fest;
+    }
+
+    public Artista getArtista() {
+        return artista;
+    }
+
+    public void setArtista(Artista artista) {
+        this.artista = artista;
+    }
+
+    public int getNumeroPres() {
+        return numeroPres;
+    }
+
+    public void setNumeroPres(int numeroPres) {
+        this.numeroPres = numeroPres;
+    }
+
+    @Override
+    public String toString() {
+        return "Presentacion{" + "fest=" + fest + ", artista=" + artista + ", numeroPres=" + numeroPres + '}';
+    }
+    
     
     
 }
