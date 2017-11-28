31514168969be40e306cdab200fbcec650a94190
User <User@172.16.223.58>
2017-11-28 08:24:34 -0500

    crea clase Asistente Festival

--- ExamenParcial01/src/examenparcial01/modelo/Boleto.java
+++ ExamenParcial01/src/examenparcial01/modelo/Boleto.java
@@ -10,5 +10,55 @@ package examenparcial01.modelo;
  * @author User
  */
 public class Boleto {
+    private String nAsiento;
+    private double costo;
+    private Asistente asis;
+    private Festival fest;
+
+    public Boleto(String nAsiento, double costo, Asistente asis, Festival fest) {
+        this.nAsiento = nAsiento;
+        this.costo = costo;
+        this.asis = asis;
+        this.fest = fest;
+    }
+
+    public String getnAsiento() {
+        return nAsiento;
+    }
+
+    public void setnAsiento(String nAsiento) {
+        this.nAsiento = nAsiento;
+    }
+
+    public double getCosto() {
+        return costo;
+    }
+
+    public void setCosto(double costo) {
+        this.costo = costo;
+    }
+
+    public Asistente getAsis() {
+        return asis;
+    }
+
+    public void setAsis(Asistente asis) {
+        this.asis = asis;
+    }
+
+    public Festival getFest() {
+        return fest;
+    }
+
+    public void setFest(Festival fest) {
+        this.fest = fest;
+    }
+
+    @Override
+    public String toString() {
+        return "Boleto{" + "nAsiento=" + nAsiento + ", costo=" + costo + ", asis=" + asis + ", fest=" + fest + '}';
+    }
+    
+    
     
 }
---
+++ ExamenParcial01/src/examenparcial01/modelo/Festival.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examenparcial01.modelo;
+
+/**
+ *
+ * @author User
+ */
+class Festival {
+    
+    private String nombre;
+    private String fecha;
+    private String lugar;
+
+    public Festival(String nombre, String fecha, String lugar) {
+        this.nombre = nombre;
+        this.fecha = fecha;
+        this.lugar = lugar;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getLugar() {
+        return lugar;
+    }
+
+    public void setLugar(String lugar) {
+        this.lugar = lugar;
+    }
+
+    @Override
+    public String toString() {
+        return "Festival{" + "nombre=" + nombre + ", fecha=" + fecha + ", lugar=" + lugar + '}';
+    }
+    
+    
+}
