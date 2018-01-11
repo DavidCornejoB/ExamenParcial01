7118940a5ace8419cb48cdbe9f8d0bd081bccff4
User <User@172.16.223.58>
2018-01-10 20:37:49 -0500

    creacion de los metodos de la libreria lista

---
+++ Practica02.03Libreria/src/libreria/Clase.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package libreria;
+
+/**
+ *
+ * @author User
+ */
+public class Clase {
+    public static String autor(){
+        return "Pablo Malla";
+    }
+    public static String fecha(){
+        return "03/01/2017";
+    }
+}
